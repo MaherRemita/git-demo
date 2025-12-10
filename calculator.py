94fea4448249f374c960a32df3702ff3a1b343e5
oussma <oussama.chahoud@univ-skikda.dz>
2025-12-10 14:11:55 +0100

    from oussama , add division

--- calculator.py
+++ calculator.py
@@ -6,3 +6,8 @@ def subtract(x, y):
     """Function to subtract two numbers"""
     return x - y
 
+def division(x, y):
+    """Function to divide two numbers"""
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    return x / y
